49f720d5ff07c29c5d4571b3598069cdecfc15ac
Matic Zavadlal <matic.zavadlal@gmail.com>
2025-08-20 17:45:41 +0100

    Update tasks.py

--- src/browser_use_sdk/resources/tasks.py
+++ src/browser_use_sdk/resources/tasks.py
@@ -1281,6 +1281,8 @@ class AsyncTasksResource(AsyncAPIResource):
         task_id: str,
         interval: float = 1,
         *,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
         extra_headers: Headers | None = None,
         extra_query: Query | None = None,
         extra_body: Body | None = None,
